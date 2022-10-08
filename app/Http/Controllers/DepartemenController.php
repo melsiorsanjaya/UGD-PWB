4e532525ee09ecd2a11e8a6a0fd6d7be88839e86
melsiorsanjaya <bandasoojayaa@gmail.com>
2022-10-08 12:37:41 +0700

    UGD 6

--- app/Http/Controllers/DepartemenController.php
+++ app/Http/Controllers/DepartemenController.php
@@ -2,22 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use Mail;
+use App\Mail\DepartemenMail; 
+use App\Models\Departemen; 
 use Illuminate\Http\Request;
-use App\Models\Departemen;
-
 
 class DepartemenController extends Controller
 {
-   /**
+/**
 * index
 *
 * @return void
 */
 public function index()
 {
- $departemen = Departemen::get();
 
- return view('departemen.index', compact('departemen'));
- }
+
+$departemen = Departemen::latest()->paginate(5);
+
+return view('departemen.index', compact('departemen'));
 }
+/**
+* create
+*
+* @return void
+*/
+public function create()
+{
+return view('departemen.create');
+}
+/**
+* store
+*
+* @param Request $request
+* @return void
+*/
+public function store(Request $request)
+{
 
+$this->validate($request, [
+'nama_departemen' => 'required',
+'nama_manager' => 'required',
+'jumlah_pegawai' => 'required'
+]);
+
+Departemen::create([
+'nama_departemen' => $request->nama_departemen,
+'nama_manager' => $request->nama_manager,
+'jumlah_pegawai' => $request->jumlah_pegawai
+]);
+try{
+
+$content = [
+'body' => $request->nama_departemen,
+];
+
+
+Mail::to('emailtujuan@gmail.com')->send(new
+DepartemenMail($content));
+
+return redirect()->route('departemen.index')->with(['success'
+=> 'Data Berhasil Disimpan, email telah terkirim!']);
+}catch(Exception $e){
+
+return redirect()->route('departemen.index')->with(['success'
+=> 'Data Berhasil Disimpan, namun gagal mengirim email!']);
+}
+}
+}
--- resources/views/departemen/index.blade.php
+++ resources/views/departemen/index.blade.php
@@ -3,7 +3,6 @@
 <div class="content-header">
 <div class="container-fluid">
 <div class="row mb-2">
-
 <div class="col-sm-6">
 <h1 class="m-0">Departemen</h1>
 </div>
@@ -11,7 +10,7 @@
 <div class="col-sm-6">
 <ol class="breadcrumb float-sm-right">
 <li class="breadcrumb-item">
-<a href="{{ url('departemen')}}">Departemen</a>
+<a href="#">Departemen</a>
 </li>
 <li class="breadcrumb-item active">Index</li>
 </ol>
@@ -30,6 +29,8 @@
 <div class="col-12">
 <div class="card">
 <div class="card-body">
+<a href="{{ route('departemen.create') }}"
+class="btn btn-md btn-success mb-3">TAMBAH DEPARTEMEN</a>
 <div class="table-responsive p-0">
 <table class="table table-hover text-nowrap">
 <thead>
@@ -38,8 +39,10 @@
 Departemen</th>
 <th class="text-center">Nama 
 Manger</th>
+
 <th class="text-center">Jumlah 
 Pegawai</th>
+<th class="text-center">Aksi</th>
 </tr>
 </thead>
 <tbody>
@@ -51,7 +54,18 @@ $item->nama_departemen }}</td>
 $item->nama_manager }}</td>
 <td class="text-center">{{ 
 $item->jumlah_pegawai }}</td>
-
+<td class="text-center">
+<form onsubmit="return
+confirm('Apakah Anda Yakin ?');" action="{{ route('departemen.destroy', 
+$item->id) }}" method="POST">
+<a href="{{ 
+route('departemen.edit', $item->id) }}" class="btn btn-sm btn-primary">EDIT</a>
+@csrf
+@method('DELETE')
+<button type="submit"
+class="btn btn-sm btn-danger">Hapus</button>
+</form> 
+</td>
 </tr>
 @empty
 <div class="alert alert-danger">
@@ -61,11 +75,13 @@ Data Departemen belum tersedia
 </tbody>
 </table>
 </div>
+<div class="d-flex justify-content-center">{{$departemen->links()}}</div>
 </div>
 <!-- /.card-body -->
 </div>
 <!-- /.card -->
 </div>
+
 <!-- /.col-md-6 -->
 </div>
 <!-- /.row -->
